6864db6503a6a07761f5ae55ec2f37063753c595
dandymau28 <hello.dandymaulana@gmail.com>
2022-05-31 22:58:34 +0700

    mod: add mongodb connection

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var express = require('express');
 var path = require('path');
 // var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes');
 var middleware = require('./middleware/api_auth');
 
 var app = express();
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,6 +18,14 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('api/v1', middleware.apiAuth, indexRouter);
+app.use('/api/v1', middleware.apiAuth, indexRouter);
+
+mongoose.connect('mongodb+srv://mongodb:VY5jAiyr9aSf2XPJ@cluster0.8m3glek.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch(err => {
+    console.log("MongoDB fail to connect", err);
+});
 
 module.exports = app;
