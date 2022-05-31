103629450d16faf6f16f432e6dff4c8620ff588e
dandymau28 <hello.dandymaulana@gmail.com>
2022-05-31 23:00:08 +0700

    mod: modify user routes location - add "no route" to handle all route

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var userRouter = require('../modules/users/users_routes');
+var userRouter = require('../src/modules/users/users_routes');
 
 router.use('/users', userRouter)
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.use('/', function(req, res, next) {
+  res.status(404).json({message: 'no route found'});
 });
 
 module.exports = router;
