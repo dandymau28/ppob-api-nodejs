3cc486a6091ce8f322c24ab686bea0148834e29f
dandymau28 <hello.dandymaulana@gmail.com>
2022-05-31 22:59:15 +0700

    mod: add created http code to allow post response

--- response/http_code.js
+++ response/http_code.js
@@ -1,4 +1,5 @@
 const SUCCESS = 200;
+const CREATED = 201;
 const BAD_REQUEST = 400;
 const NOT_FOUND = 404;
 const CONFLICT = 409;
@@ -8,6 +9,7 @@ const INTERNAL_SERVER_ERROR = 500;
 
 module.exports = {
     SUCCESS,
+    CREATED,
     BAD_REQUEST,
     NOT_FOUND,
     CONFLICT,
