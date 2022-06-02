355fefe75ff4a46c9243e196362c31f7e9f25487
dandymau28 <hello.dandymaulana@gmail.com>
2022-06-02 20:08:30 +0700

    fix: reduce minimum phone length

--- src/modules/users/users_validators.js
+++ src/modules/users/users_validators.js
@@ -2,14 +2,14 @@ const { body, param, query } = require('express-validator');
 
 const validators = {
     register: [
-        body('no_handphone').isLength({ min: 13, max:15 }).withMessage('Phone number must be at least 13 characters long'),
+        body('no_handphone').isLength({ min: 11, max:14 }).withMessage('Phone number must be at least 11 characters long'),
         body('mac_address').exists({ checkNull: true }).withMessage('Mac address is required')
     ],
     generateOTP: [
-        param('phone').isLength({ min:13, max:15 }).withMessage('Phone number must be at least 13 characters long')
+        param('phone').isLength({ min:11, max:14 }).withMessage('Phone number must be at least 11 characters long')
     ],
     verifyOTP: [
-        param('phone').isLength({ min:13, max:15 }).withMessage('Invalid phone number'),
+        param('phone').isLength({ min:11, max:14 }).withMessage('Invalid phone number'),
         query('otp').isLength({ min:6, max:6 }).withMessage('Invalid OTP')
     ]
 };
