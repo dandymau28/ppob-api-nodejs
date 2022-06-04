d190ee091b464808cc9c50313eb417f2a7a49f45
dandymau28 <hello.dandymaulana@gmail.com>
2022-06-04 14:34:21 +0700

    mod: sort http code by code

--- response/http_code.js
+++ response/http_code.js
@@ -1,10 +1,10 @@
 const SUCCESS = 200;
 const CREATED = 201;
 const BAD_REQUEST = 400;
-const NOT_FOUND = 404;
-const CONFLICT = 409;
 const UNAUTHORIZED = 401;
 const FORBIDDEN = 403;
+const NOT_FOUND = 404;
+const CONFLICT = 409;
 const INTERNAL_SERVER_ERROR = 500;
 
 module.exports = {
