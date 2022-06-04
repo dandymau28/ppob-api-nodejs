dcff70c4bc99f629b3ce95825a6673b1dd76187a
dandymau28 <hello.dandymaulana@gmail.com>
2022-06-04 14:35:46 +0700

    mod: add profile and refresh token route

--- src/modules/users/users_routes.js
+++ src/modules/users/users_routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const userController = require("./users_controllers");
 const userValidator = require("./users_validators");
-// const { reqAuth } = require("../../middleware/req_auth");
+const { reqAuth } = require("../../../middleware/req_auth");
 
 router.post('/register', userValidator.register, userController.register);
-router.patch('/:phone/generate-otp', userValidator.generateOTP, userController.generateOTP);
-router.get('/:phone/verify-otp', userValidator.verifyOTP, userController.verifyOTP);
+router.post('/:phone/generate-otp', userValidator.generateOTP, userController.generateOTP);
+router.patch('/:phone/verify-otp', userValidator.verifyOTP, userController.verifyOTP);
+router.get('/:phone/profile', reqAuth, userController.getProfile);
+router.post('/:phone/refresh-token', reqAuth, userController.refreshToken);
 router.head('/:type/check/:id', userController.checkID);
 
 module.exports = router;
