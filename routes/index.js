8a68c4efd1f662becb2dfcac636648a47de389b4
dandymau28 <hello.dandymaulana@gmail.com>
2022-07-01 12:38:17 +0700

    mod: update product routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var userRouter = require('../src/modules/users/users_routes');
+var productRouter = require('../src/modules/products/products_routes');
 
 router.use('/users', userRouter)
+router.use('/products', productRouter)
 
 router.use('/', function(req, res, next) {
   res.status(404).json({message: 'no route found'});
