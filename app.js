88dec7ab5fdd88233fd79f20725554cda7198e5f
dandymau28 <hello.dandymaulana@gmail.com>
2022-07-01 12:37:27 +0700

    mod: modifying app js
    - add request id
    - add log at request and response
    - run telegram bot

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var path = require('path');
 // var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var logger = require('morgan');
+var logger = require('./helper/logger')
 var mongoose = require('mongoose');
 var cors = require('cors');
-
+var { v4: uuidv4 } = require('uuid');
 var indexRouter = require('./routes');
 var middleware = require('./middleware/api_auth');
+var startBot = require('./telegram')
 
 var app = express();
 
 app.use(cors());
 
-app.use(logger('dev'));
+app.use(function(req, res, next) {
+    req.id = uuidv4();
+    next();
+})
+
 app.use(express.json());
+app.use(logger.request)
+app.use(logger.response)
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -27,6 +35,8 @@ mongoose.connect('mongodb+srv://mongodb:VY5jAiyr9aSf2XPJ@cluster0.8m3glek.mongod
 .catch(err => {
     console.log("MongoDB fail to connect", err);
 });
+
+startBot();
 
 mongoose.connection.on("disconnected", () => {
     console.log("Disconnected gracefully");
