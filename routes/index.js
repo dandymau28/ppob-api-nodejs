a7a5b9e7b5f43ac43ca96db4d9d59ba22fbe06e3
dandymau28 <hello.dandymaulana@gmail.com>
2022-07-11 18:22:12 +0700

    mod: add purchase routes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ var router = express.Router();
 
 var userRouter = require('../src/modules/users/users.routes');
 var productRouter = require('../src/modules/products/products.routes');
+var purchaseRouter = require('../src/modules/purchases/purchases.routes')
 
 router.use('/users', userRouter)
 router.use('/products', productRouter)
+router.use('/purchases', purchaseRouter)
 
 router.use('/', function(req, res, next) {
   res.status(404).json({message: 'no route found'});
