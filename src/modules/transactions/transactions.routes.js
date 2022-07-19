68cabe8ed75c246ee89852248557c46bb4a54df7
dandymau28 <hello.dandymaulana@gmail.com>
2022-07-19 15:24:59 +0700

    mod: several updates
    1. add validator to purchase product
    2. add route validate number

--- src/modules/transactions/transactions.routes.js
+++ src/modules/transactions/transactions.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const transactionController = require('../transactions/transactions.controllers')
+const transactionValidator = require('../transactions/transactions.validator')
 const { reqAuth } = require('../../../middleware/req_auth')
 
 router.get('/product/:id/detail', transactionController.getTransactionDetail);
-router.post('/product/:id/buy', reqAuth, transactionController.purchaseProduct);
+router.post('/product/:id/buy', reqAuth, transactionValidator.purchaseProduct, transactionController.purchaseProduct);
+router.get('/product/:category/validate/:customerNo', reqAuth, transactionValidator.validateID, transactionController.validateID);
 
 module.exports = router;
