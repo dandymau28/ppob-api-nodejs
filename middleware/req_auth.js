d3e5b0c4530e8627f1c8ecf2894ae3aa9d8eab03
dandymau28 <hello.dandymaulana@gmail.com>
2022-07-27 16:39:09 +0700

    mod: bug fix token

--- middleware/req_auth.js
+++ middleware/req_auth.js
@@ -19,7 +19,10 @@ module.exports.reqAuth = async(req, res, next) => {
 
         // let [phone, token] = decodedToken.split(':');    
 
-        let user = await Users.findOne({ noHandphone: phone, token: authToken });
+        // let user = await Users.findOne({ noHandphone: phone, token: authToken });
+        let user = await Users.findOne({ token: authToken });
+
+        console.log(user);
 
         if (!user) {
             return response.error(res, http.UNAUTHORIZED, null, 'Credentials invalid');
