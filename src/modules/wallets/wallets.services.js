79bf4cd0072f58c9fe316c5a27f2c915f34c4e66
dandymau28 <hello.dandymaulana@gmail.com>
2022-08-16 20:23:08 +0700

    fix: bug typo

--- src/modules/wallets/wallets.services.js
+++ src/modules/wallets/wallets.services.js
@@ -3,7 +3,7 @@ const processQueue = require('../../../queue/process')
 
 const service = {
     getBalanceByPhone: async(phone) => {
-        return await Wallets.findOne({ phone}).select({ user:1, balance:1, _id: 0})
+        return await Wallets.findOne({ user: {phone}}).select({ user:1, balance:1, _id: 0})
     },
     processTransaction: ({ phone: phone, pay: amount}) => {
         processQueue({ phone: phone, pay: amount})
