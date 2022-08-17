d8d2da0fed607564159a4d429a1c508c54756cb6
dandymau28 <hello.dandymaulana@gmail.com>
2022-08-17 20:01:26 +0700

    mod: add validation

--- src/modules/wallets/wallets.routes.js
+++ src/modules/wallets/wallets.routes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const walletController = require('./wallets.controllers')
+const walletValidator = require('./wallets.validators')
 
-router.get('/:phone/check/balance', walletController.checkBalance)
-router.post('/:phone/topup/balance', walletController.topupBalance)
-router.get('/:phone/history')
+router.get('/:phone/check/balance', walletValidator.checkBalance, walletController.checkBalance)
+router.post('/:phone/topup/balance', walletValidator.topupBalance, walletController.topupBalance)
+router.get('/:phone/history', walletValidator.history, walletController.getWalletHistory)
 
 module.exports = router
