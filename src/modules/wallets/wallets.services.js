217c1ed398d9e639b240bbcd227ed8aa58db4b18
dandymau28 <hello.dandymaulana@gmail.com>
2022-08-17 20:02:00 +0700

    mod: add process transaction and get history wallet

--- src/modules/wallets/wallets.services.js
+++ src/modules/wallets/wallets.services.js
@@ -1,12 +1,16 @@
 const Wallets = require('../../models/wallets')
+const WalletHistory = require('../../models/walletHistory')
 const processQueue = require('../../../queue/process')
 
 const service = {
     getBalanceByPhone: async(phone) => {
         return await Wallets.findOne({ user: {phone}}).select({ user:1, balance:1, _id: 0})
     },
-    processTransaction: ({ phone: phone, pay: amount}) => {
-        processQueue({ phone: phone, pay: amount})
+    processTransaction: (item) => {
+        processQueue(item)
+    },
+    getHistoryByPhone: async(phone) => {
+        return await WalletHistory.find({ phone }).select({ _id: 0}).sort({ txnAt: -1 })
     }
 }
 
