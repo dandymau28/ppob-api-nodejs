3ca59d618e27bfe3a52082ec00e4088f321fb552
dandymau28 <hello.dandymaulana@gmail.com>
2022-08-17 20:01:17 +0700

    mod: add validation and wallet history

--- src/modules/wallets/wallets.controllers.js
+++ src/modules/wallets/wallets.controllers.js
@@ -1,10 +1,17 @@
 const response = require('../../../response')
 const http = require('../../../response/http_code')
 const walletService = require('./wallets.services')
+const { validationResult } = require('express-validator');
 
 const controllers = {
     checkBalance: async(req, res) => {
         try {
+            const errors = validationResult(req);
+    
+            if (!errors.isEmpty()) {
+                return response.error(res, http.BAD_REQUEST, errors.array(), 'Invalid request');
+            }
+
             let { phone } = req.params
 
             if (!phone) {
@@ -24,6 +31,12 @@ const controllers = {
     },
     topupBalance: async(req, res) => {
         try {
+            const errors = validationResult(req);
+    
+            if (!errors.isEmpty()) {
+                return response.error(res, http.BAD_REQUEST, errors.array(), 'Invalid request');
+            }
+
             let { phone } = req.params
             let { amount } = req.body
 
@@ -33,6 +46,27 @@ const controllers = {
         } catch (err) {
             console.error(err)
             return response.error(res, http.INTERNAL_SERVER_ERROR, err,'InternalError')
+        }
+    },
+    getWalletHistory: async(req, res) => {
+        try {
+            const errors = validationResult(req);
+    
+            if (!errors.isEmpty()) {
+                return response.error(res, http.BAD_REQUEST, errors.array(), 'Invalid request');
+            }
+
+            let { phone } = req.params
+
+            let histories = await walletService.getHistoryByPhone(phone)
+
+            if (!histories) {
+                return response.error(res, http.NOT_FOUND, 'no wallet found', 'not found')
+            }
+
+            return response.success(res, histories, 'history found')
+        } catch (err) {
+            return response.error(res, http.INTERNAL_SERVER_ERROR,  err, 'InternalError')
         }
     }
 }
