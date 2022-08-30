7139ab4815d254c9269866566de6005fe0878c86
dandymau28 <hello.dandymaulana@gmail.com>
2022-08-30 12:02:16 +0700

    mod: set pin validator

--- src/modules/users/users.validators.js
+++ src/modules/users/users.validators.js
@@ -11,6 +11,10 @@ const validators = {
     verifyOTP: [
         param('phone').isLength({ min:11, max:14 }).withMessage('Invalid phone number'),
         query('otp').isLength({ min:6, max:6 }).withMessage('Invalid OTP')
+    ],
+    setPIN: [
+        body('new_pin').isLength({ min:6, max:6 }).withMessage('PIN must be 6 characters long'),
+        body('new_pin_validation').isLength({ min:6, max:6 }).withMessage('PIN must be 6 characters long')
     ]
 };
 
