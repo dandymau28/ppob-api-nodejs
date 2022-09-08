1764cfed1ca78101db0b486d93ef24ea0bfd927b
dandymau28 <hello.dandymaulana@gmail.com>
2022-09-08 08:54:08 +0700

    mod: handle if any hook request

--- middleware/api_auth.js
+++ middleware/api_auth.js
@@ -3,18 +3,23 @@ const http = require('../response/http_code');
 
 module.exports.apiAuth = (req, res, next) => {
     const clientKey = req.headers['x-api-key'];
+    const digiflazzDelivery = req.headers['x-digiflazz-delivery'];
+    // const hookSecret = process.env.HOOK_SECRET
 
     try {
-        //check if api key exist
-        if (!clientKey) {
-            return response.error(res, http.UNAUTHORIZED, null, 'Invalid API Key');
-        }
-
-        let serverKey = process.env.API_KEY;
-        
-        //compare api key
-        if (serverKey !== clientKey) {
-            return response.error(res, http.UNAUTHORIZED, null, 'Invalid API Key');
+        if (!digiflazzDelivery) {
+            //check if api key exist
+            if (!clientKey) {
+                return response.error(res, http.UNAUTHORIZED, null, 'Invalid API Key');
+            }
+    
+            let serverKey = process.env.API_KEY;
+            
+            //compare api key
+            if (serverKey !== clientKey) {
+                return response.error(res, http.UNAUTHORIZED, null, 'Invalid API Key');
+            }
+            
         }
 
         next();
